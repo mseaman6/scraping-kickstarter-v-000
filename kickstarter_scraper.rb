7257d6dfa85da7792e91e5dceb47dd37333ff613
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-28 18:17:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  binding.pry
 end
 
 create_project_hash
@@ -15,4 +14,4 @@ create_project_hash
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("span.location-name").text
-#percent_funded: project.css("ul.project-stats li.first.funded strong").text
+#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
